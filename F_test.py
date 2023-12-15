a85dc95209e86bfb83b3e7e2e2b8cfe5971ded68
Igortigr <liigor.acer@gmail.com>
2023-12-15 12:40:25 +0300

    add file F_test.py

---
+++ F_test.py
@@ -0,0 +1,8 @@
+def f_test(x, y):
+    '''Функция, рассчитывающая статистику F-теста и соответсвующее значение p_value'''
+
+    f = np.var(x, ddof=1) / np.var(y, ddof=1)
+    dfn = x.size - 1
+    dfd = y.size - 1
+    p = 1 - sts.f.cdf(f, dfn, dfd)
+    return f, p
